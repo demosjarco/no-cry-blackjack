85a0350176a192cc2a8564dacb1317a393795444
demosjarco <demosjarco@users.noreply.github.com>
2019-08-06 22:54:01 -0700

    maybe?

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 "use strict";
 
-const Pool = require('worker-threads-pool')
-const wtp = new Pool({ max: require('os').cpus().length });
-const { SHARE_ENV } = require('worker_threads');
+const { Worker, SHARE_ENV } = require('worker_threads');
 
 require('dotenv').config();
 // process.env.VARIABLE
@@ -14,19 +12,18 @@ app.listen(process.env.WEBSOCKET_PUBLIC_PORT);
 
 io.on('connection', function (socket) {
 	console.log('test 1 connected');
-	wtp.acquire('./threads/public/createLobby.js', { env: SHARE_ENV }, function (err, worker) {
-		worker.on('error', (err2) => {
-			console.error(err2);
-		});
-		worker.on('message', (value) => {
-			console.log(value);
-		});
-		worker.on('close', () => {
-			console.log('closed');
-		});
-		socket.on('createRoom', function (gameInfo) {
-			console.log(gameInfo);
-			//worker.postMessage({ createRoom: gameInfo });
-		});
+	const playerThread = new Worker('./threads/public/createLobby.js', { env: SHARE_ENV });
+	playerThread.on('error', (err2) => {
+		console.error(err2);
+	});
+	playerThread.on('message', (value) => {
+		console.log(value);
+	})
+	playerThread.on('close', () => {
+		console.log('closed');
+	});
+	socket.on('createRoom', function (gameInfo) {
+		//console.log(gameInfo);
+		playerThread.postMessage({ createRoom: gameInfo });
 	});
 });
